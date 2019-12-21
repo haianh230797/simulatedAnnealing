b98cb66cf91634a7ff4319057dc61830125cb2cb
haianh230797 <haianh230797@gmail.com>
2019-12-21 23:30:24 +0700

    add simulated annealing function

--- src/SimAne.java
+++ src/SimAne.java
@@ -1,8 +1,5 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
 
 public class SimAne {
 
@@ -11,120 +8,97 @@ public class SimAne {
         System.out.println(list);
 
         Random rd = new Random();
-        int rda = rd.nextInt();
-        int rdb = rd.nextInt();
-        int rdc = rd.nextInt();
+        int rda;
+        int rdb;
+        int rdc;
 
         Object[] arr = list.toArray(); //convert to array
-        for (int i = 0; i < arr.length; i++) {
-            int fitnessValueTargetBranch = arr[i].toString().length();  //length of element in 1 path
-//            System.out.println(arr[i]);
-//            System.out.println(fitnessValueTargetBranch);
-            int T = 0;
+        for (Object o : arr) {
+            int fitnessValueTargetBranch = o.toString().length();  //length of element in 1 path
+
+            int T;
             T = 0;
             while (T < 1000) {
-                rda = rd.nextInt(10);
-                rdb = rd.nextInt(10);
-                rdc = rd.nextInt(10);
+                rda = rd.nextInt(10000);
+                rdb = rd.nextInt(10000);
+                rdc = rd.nextInt(10000);
                 Triangle.newTrace();
                 Triangle.checkTriangle(rda, rdb, rdc);
 
                 StringBuilder sb = new StringBuilder();
                 for (Object obj : Triangle.getTrace()) {
+
+
+
                     sb.append(obj.toString());
                 }
 
 
-
-                String str1 = arr[i].toString();
+                String str1 = o.toString();
                 String str2 = sb.toString();
-                String str11 = "";
-                String str12 = "";
+                StringBuilder str11 = new StringBuilder();
+                StringBuilder str12 = new StringBuilder();
                 char[] ch1 = str1.toCharArray();
                 char[] ch2 = str2.toCharArray();
                 int count = 0;
-                for(int j=0; j<ch1.length; j++)
-                {
-                    if(!str11.contains(ch1[j]+""))
-                    {
-                        str11 += ch1[j];
+                for (char item : ch1) {
+                    if (!str11.toString().contains(item + "")) {
+                        str11.append(item);
                     }
                 }
-                for(int j=0; j<ch2.length; j++)
-                {
-                    if(!str12.contains(ch2[j]+""))
-                    {
-                        str12 += ch2[j];
+                for (char value : ch2) {
+                    if (!str12.toString().contains(value + "")) {
+                        str12.append(value);
                     }
                 }
-                char[] ch11 = str11.toCharArray();
-                char[] ch12 = str12.toCharArray();
-                for(int j=0; j<ch11.length; j++)
-                {
-                    for(int k=0; k<ch12.length; k++)
-                    {
-                        if(ch11[j] == ch12[k])
-                        {
+                char[] ch11 = str11.toString().toCharArray();
+                char[] ch12 = str12.toString().toCharArray();
+                for (char value : ch11) {
+                    for (char c : ch12) {
+                        if (value == c) {
                             count++;
                         }
                     }
                 }
-                if(count == fitnessValueTargetBranch ){
-                    System.out.println("test case cua path " + arr[i] + " la: " + rda+ "  " + rdb+ "  " +rdc+ "  "  );
-                break;
+                if (count == fitnessValueTargetBranch) {
+                    System.out.println("test case cua path " + o + " la: " + rda + "  " + rdb + "  " + rdc + "  ");
+                    break;
+                }
+                int temp = 0;
+                int tempa;
+                int tempb;
+                int tempc;
+                int Freeze = 5;
+                if(count < fitnessValueTargetBranch){
+                    if(count > temp){
+                        temp = count;
+                        tempa = rda;
+                        tempb = rdb;
+                        tempc = rdc;
+                        continue;
+                    }
+                    if(count < temp ){
+                        if(Freeze == 0){
+                            continue;
+                        }
+                        temp = count;
+                        tempa = rda;
+                        tempb = rdb;
+                        tempc = rdc;
+                        Freeze--;
+                        continue;
+                    }
+
                 }
 
                 T++;
+                if(T==999 && count < fitnessValueTargetBranch ){
+                    System.out.println("test case cua path " + o + " la: " + rda + "  " + rdb + "  " + rdc + "  ");
+
+                }
             }
         }
-
-
-//        for( Branch path : list){
-//
-//            int T=0;
-//            T = 0;
-//            while( T <=10){
-//                int fitnessValueTargetBranch = String.valueOf(path).length(); //length of path
-//                rda = rd.nextInt();
-//                rdb = rd.nextInt();
-//                rdc = rd.nextInt();
-//
-//
-//            Triangle.newTrace();
-//            Triangle.checkTriangle(rda,rdb,rdc);
-//
-////            int fitnessValueChromosomeBranch = Triangle.getTrace().size();
-//
-//            String s1 = path.toString();
-//            String[] words=s1.split("");
-//            int fitnessValueChromosomeBranch =0;
-//
-//            for(int i=0; i<words.length;i++) {
-//
-//                Object[] arr2 = Triangle.getTrace().toArray();
-//                for (int j = 0; j <= arr2.length; j++) {
-//
-//                    if (arr2[j] == words[i]) {
-//
-//                        fitnessValueChromosomeBranch++;
-//                        break;
-//                    }
-//                    if (arr2[j] != words[i]) {
-//                        break;
-//                    }
-//                }
-//
-//            }
-//            if(fitnessValueTargetBranch == fitnessValueChromosomeBranch || fitnessValueTargetBranch< fitnessValueChromosomeBranch ){
-//                System.out.println("test case cua path " + path+ " la " + rda+ "  " + rdb+ "  " +rdc+ "  "  );
-//                break;
-//            }
-//            T++;
-//        }
-//        }
-
     }
-
 }
 
 
